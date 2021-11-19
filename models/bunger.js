07938f46a71a757a63e2396f02fabc3c5dbea9ff
Karen <kyleehrenfeld@gmail.com>
2021-11-19 16:23:12 -0700

    end day 2

---
+++ models/bunger.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const bungerSchema = new mongoose.Schema({
+  bunger:{
+    img: { 
+      required: [true, "please provide a img"], 
+      Type: String, 
+      unique: true,
+    },
+    Reputation: {
+      Type: String,
+      default: '0'
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    required: [true, "please provide a img"], 
+  }, timestamps: true });
+
+
+module.exports = mongoose.model("bunger", bungerSchema);
---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    required: [true, "please provide a name"],
+    minlength: 3,
+    maxlength:50
+  },
+  email:{
+    type: String,
+    required: [true, "please provide a email"],
+    unique: true,
+    lowercase: true,
+    match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, "must provide a valid email"]
+  },
+  password:{
+    type: String,
+    required: [true, "please provide a password"],
+    minlength: 6
+  },
+});
+
+userSchema.pre("save", async function (next){
+  this.password = await bcrypt.hash(this.password, await bcrypt.genSalt(10));
+  next();
+});
+userSchema.methods.createJWT = function () {
+  return jwt.sign(
+    {userID: this._id, name: this.name},
+    process.env.JWT_SIGNATURE,
+    {
+      expiresIn: "30d"
+    }
+  )
+}
+userSchema.methods.comparePassword = async function (submittedPass) {
+  const isMatch = await bcrypt.compare(submittedPass, this.password);
+  return isMatch;
+};
+
+
+
+module.exports = mongoose.model("User", userSchema);
+
