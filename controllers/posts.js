57e095b70b79077272352d0a655a18d66a271839
buni <alisson.corrales24@gmail.com>
2021-11-23 17:13:50 -0700

    did post and the find singels

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,50 +1,55 @@
-const { StatusCodes } = require("http-status-codes")
+const { StatusCodes } = require("http-status-codes");
 const Bung = require("../models/bunger");
+const NotFound = require("../errors/notFound")
 //const User = require("../models/user")
 
-
-
-
 //🍔
-const postPost = async (req, res) =>{
-    const dude = req.user.userID
-    console.log(req.body);
-    req.body.createdBy = dude
-    const bunger = await Bung.create(req.body);
+const postPost = async (req, res) => {
+  const dude = req.user.userID;
+  console.log(req.body);
+  req.body.createdBy = dude;
+  const bunger = await Bung.create(req.body);
 
-    res.status(StatusCodes.CREATED).json({ bunger });
-}
-
-//🍔
-const getOnePost = (req, res) =>{
-    res.send("this works")
-}
-
-
-//🍔
-const getAllPosts = async (req, res) =>{
-    //const { userID } = req.user.userID;
-
-    const bung =  await Bung.find({ createdBy: req.user.userID }).sort(
-        "created at"
-    )
-    res.status(StatusCodes.OK).json({ bung, length: bung.length });
+  res.status(StatusCodes.CREATED).json({ bunger });
 };
 
+//🍔
+const getOnePost = async (req, res) => {
+  //const { userID } = req.user.userID;
+  const { id: jobID } = req.params;
+
+  const bung = await Bung.findOne({ createdBy: req.user.userID, _id: jobID });
+
+  if (!bung) {
+    throw new NotFound(`not a post with ${jobID}, doofus`);
+  }
+
+  res.status(StatusCodes.OK).json({ bung });
+};
 
 //🍔
-const editPost = (req, res) =>{
-    res.send("this works")
-}
+const getAllPosts = async (req, res) => {
+  //const { userID } = req.user.userID;
+
+  const bung = await Bung.find({ createdBy: req.user.userID }).sort(
+    "created at"
+  );
+  res.status(StatusCodes.OK).json({ bung, length: bung.length });
+};
 
 //🍔
-const deleteAllPosts = (req, res) =>{
-    res.send("this works")
-}
+const editPost = (req, res) => {
+  res.send("this works");
+};
 
 //🍔
-const deletePost = (req, res) =>{
-    res.send("this works")
-}
+const deletePost = (req, res) => {
+};
 
-module.exports = { postPost, getOnePost, getAllPosts, editPost, deleteAllPosts, deletePost }
+module.exports = {
+  postPost,
+  getOnePost,
+  getAllPosts,
+  editPost,
+  deletePost,
+};
--- errors/notFound.js
+++ errors/notFound.js
@@ -1,5 +1,11 @@
-const NotFound = (req, res) => {
-    res.status(404).json({ msg: 'page not found :(' });
+const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("./customAPI");
+
+class NotFound extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.StatusCodes = StatusCodes.NOT_FOUND;
+  }
 }
 
-module.exports = NotFound
+module.exports = NotFound;
